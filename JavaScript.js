2cbae5c4d2d2616280de71c008dfcc69f668af84
Dima <dima.kulik.2234@gmail.com>
2024-05-02 17:58:28 +0300

    new folder

---
+++ JavaScript.js
@@ -0,0 +1,23 @@
+// 'use strict';
+//
+// const arr = ['a', 'b', 'c'];
+// const arrObj = {
+//     a:'a',
+//     '1': 'b',
+//     3: 'c',
+//     b : '34231'
+//
+// }
+// arrObj.b = 1234;
+// console.log(arrObj.b);
+
+const storeName = 'Cheel';
+const storeDescription = {
+    budget : 10000,
+    employees: ['Dima', 'Sasha', 'Jek'],
+    product: {Lemon: 200,
+        cherry: 400,
+    },
+    open: true
+};
+console.log(storeDescription.employees)
